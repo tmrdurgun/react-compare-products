33b1ab38cd0212a19e0f509ffad388fb86def5a4
Tamer Durgun <tmr.durgun@gmail.com>
2021-05-22 21:42:31 +0300

    product feature style update

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -18,24 +18,16 @@ class Products extends Component {
 
     this.sidebarFeatureList = [
       "Artikelnummer",
-      "badges",
       "BUP_Conversion",
       "BUP_UOM",
       "BUP_Value",
       "channel",
-      "grossPrice",
       "Hardheid",
       "Inwendige diameter",
       "Kleur",
-      "listPrice",
       "Maat volgens AS568",
-      "manufacturerImage",
-      "manufacturerName",
       "Materiaal",
       "minQuantity",
-      "name",
-      "productImage",
-      "salePrice",
       "sku",
       "Snoerdikte",
       "stepQuantity",
@@ -60,69 +52,74 @@ class Products extends Component {
     return (
 
       <>
-      <div className="page-header">
-        <h1 className="page-title">{`${products && products.length} Producten vergelijken`}</h1>
-      </div>
-
-      <div className="products-container">
-
-        <div className="products-sidebar">
-          <h4 className="sidebar-title">Je selectie</h4>
-
-          <div className="sidebar-filter">
-            <ul className="sidebar-filter-list">
-              {products && products.map((item, i) => {
-                return (<li key={i + 1} className="sidebar-filter-list-item">
-                  <label htmlFor={`product-cb-${i + 1}`}>
-                    <input type="checkbox" name="" id={`product-cb-${i + 1}`} />
-                    <span className="text-bold">{item.name}</span>
-                    </label>
-                  
-                </li>)
-              })}
-            </ul>
-          </div>
-
-          <div className="sidebar-feature-container">
-            <ul className="feature-list">
-              { this.sidebarFeatureList.map((item, i) => {
-                return (<li key={i + 1} className="feature-list-item list-item">{item}</li>)
-              })}
-            </ul>
-          </div>
+        <div className="page-header">
+          <h1 className="page-title">{`${products && products.length} Producten vergelijken`}</h1>
         </div>
 
-        <div className="products-page">
-        {products && products.map((item, i) => (
-          <div className="product">
-            <div className="product-header">
-              <div className="product-header-picture">
-                <img src={productImg} alt="" />
-              </div>
-              <div className="product-header-name text-bold">{item.name}</div>
-              <div className="product-header-price">{item.salePrice}</div>
-              <div className="product-header-desc">per stuk / excl. btw</div>
+        <div className="products-container">
+
+          <div className="products-sidebar">
+            <h4 className="sidebar-title">Je selectie</h4>
+
+            <div className="sidebar-filter">
+              <ul className="sidebar-filter-list">
+                {products && products.map((item, i) => (
+                  <li key={i + 1} className="sidebar-filter-list-item">
+                    <label htmlFor={`product-cb-${i + 1}`}>
+                      <input type="checkbox" name="" id={`product-cb-${i + 1}`} />
+                      <span className="text-bold">{item.name}</span>
+                    </label>
+                  </li>
+                ))}
+              </ul>
             </div>
-            <div className="product-content">
-              <ul className="product-feature-list">
-                {item.sortedKeys.map((key, keyIndex) => {
-                  if(key !== 'atp' && key !== 'display') {
-                    return (
-                      <li key={keyIndex + 1} className="products-feature-list-item list-item">{item[key]}</li> 
-                    )
-                  } else {
-                    return <li></li>
-                  }
-                })}
+
+            <div className="sidebar-feature-container">
+              <ul className="feature-list">
+                {this.sidebarFeatureList.map((item, i) => (<li key={i + 1} className="feature-list-item list-item">{item}</li>))}
               </ul>
             </div>
           </div>
-          ))}
+
+          <div className="products-page">
+            {products && products.map((item, i) => (
+              <>
+                {item.display && <div className="product">
+
+                  <div className="product-header">
+                    <div className="product-header-picture">
+                      <img src={productImg} alt="" />
+                    </div>
+                    <div className="product-header-name text-bold">{item.name}</div>
+                    <div className="product-header-price">{item.salePrice}</div>
+                    <div className="product-header-desc">per stuk / excl. btw</div>
+                  </div>
+                  
+                  <div className="product-content">
+                    <ul className="product-feature-list">
+                      <li className="product-feature-list-item list-item badges"> <img src={badgesImg} alt="" /> </li>
+
+                      {item.sortedKeys.map((key, keyIndex) => (
+                        <>
+                          {(key !== 'atp' && key !== 'display' && key !== 'grossPrice' && key !== 'listPrice'
+                            && key !== 'manufacturerImage' && key !== 'manufacturerName' && key !== 'name'
+                            && key !== 'productImage' && key !== 'salePrice' && key !== 'sortedKeys' && key !== 'badges') &&
+
+                            <li key={keyIndex + 1} className="product-feature-list-item list-item">{item[key]}</li>}
+                        </>
+                      ))}
+                    </ul>
+                  </div>
+
+                </div>}
+              </>
+
+            ))}
+          </div>
         </div>
-      </div>
 
       </>
-      
+
     )
   }
 }
